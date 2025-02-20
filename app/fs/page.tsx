b4879277ee16d30559636281d15c52ebaf14a00f
Sketch Company <mtb.2008@web.de>
2025-02-20 20:29:37 +0100

    0.0.1

--- app/fs/page.tsx
+++ app/fs/page.tsx
@@ -1,30 +1,22 @@
-import config from "@/config"
 import utilities from "@/utilities"
-import fs from "fs"
-
-interface IFileSystemEntries{
-    [folders: string]: string[], 
-    files: string[], 
-    left: string[]
-}
 
 export default async function FileSystemRootPage(){
 
-    const fullPath = "/"
+const fullPath = "/"
 
-    const entries = await readFileSystemEntries(fullPath)
+    const entries = await utilities.readFileSystemEntries(fullPath)
 
     return (
         <>
             <h1>{fullPath}</h1>
             <pre>
-                folders: {parseStringArrayIntoString("files")}
+                folders: {parseStringArrayIntoString("folders")}
             </pre>
             <pre>
-                files: {entries.files}
+                files: {parseStringArrayIntoString("files")}
             </pre>
             <pre>
-                left: {entries.left}
+                left: {parseStringArrayIntoString("left")}
             </pre>
         </>
     )
@@ -32,45 +24,8 @@ export default async function FileSystemRootPage(){
     function parseStringArrayIntoString(field: string){
         let fullString: string = ""
         entries[field].forEach((value: string) => {
-            fullString += value + "\n"
+            fullString += "\n" + value
         })
         return fullString
     }
-}
-
-function readFileSystemEntries(fullPath: string){
-    return new Promise<IFileSystemEntries>(async cb => {
-        try{        
-            const mainPath = config.devDirectory + fullPath.substring(1)
-
-            console.log(fullPath, "readFileSystemEntries: mainPath", mainPath)
-        
-            if(!utilities.exists(mainPath)) await utilities.createDirectory(mainPath)
-        
-            if(utilities.exists(mainPath)){
-                const entries = await utilities.readDirectory(mainPath)
-        
-                const fullEntries: string[] = []
-        
-                entries.forEach((element: string) => {
-                    fullEntries.push(mainPath + element)
-                })
-        
-                const folders = fullEntries.filter(currentPath => fs.statSync(currentPath).isDirectory())
-                const files = fullEntries.filter(currentPath => fs.statSync(currentPath).isFile())
-                const left = fullEntries.filter(currentPath => !fs.statSync(currentPath).isDirectory()).filter(currentPath => !fs.statSync(currentPath).isFile())
-
-                console.log(fullPath, "readFileSystemEntries: fullEntries", fullEntries)
-
-                cb({
-                    folders,
-                    files,
-                    left
-                })
-            }
-        }
-        catch(err){
-            console.error(err)
-        }
-    })
 }
